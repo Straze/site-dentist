87e43334f9608732a68986ea7060086eea4fbc56
Straze <s4league_4story@yahoo.com>
2026-02-25 22:27:30 +0200

    Delete javascript directory

--- javascript/active.js
+++
@@ -1,7 +0,0 @@
-
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        const imageWithLabel = document.querySelector('.image-with-label');
-        imageWithLabel.classList.add('active');
-    }, 1000); // 10 seconds in milliseconds
-});
