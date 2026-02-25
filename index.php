200d218f771443215a3d2ac291388bad28ec20d3
Straze <s4league_4story@yahoo.com>
2026-02-25 22:27:12 +0200

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'config.php';
-if(isset($_POST["ip"])){
-  $ip = $_POST["ip"];
-
-  $query = "INSERT INTO entry_data VALUES('', '$ip', CURRENT_TIMESTAMP)";
-  mysqli_query($conn, $query);
-}
-?>
